2a380f36d5f6ad7df61a945526b2d110bc9050d9
Sérgio Ernane Munhós Hermes <hermes.sergio@gmail.com>
2017-07-11 11:33:38 -0300

    Added Module and Event Prototype

---
+++ src/FileManager/Image/Module.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Image Module
+ *
+ * @link
+ * @copyright Copyright (c) 2017
+ */
+namespace FileManager\Image;
+
+use ZF\Apigility\Provider\ApigilityProviderInterface;
+use Zend\Uri\UriFactory;
+use Zend\Mvc\MvcEvent;
+use Zend\Mvc\ModuleRouteListener;
+use ZF\MvcAuth\MvcAuthEvent;
+
+/**
+ * Module Class for image
+ *
+ * @author Sérgio Ernane Munhós Hermes <hermes.sergio@gmail.com>
+ */
+class Module implements ApigilityProviderInterface
+{
+    public function onBootstrap(MvcEvent $mvcEvent)
+    {
+        UriFactory::registerScheme('chrome-extension', 'Zend\Uri\Uri'); // add chrome-extension for API Client
+        $serviceManager = $mvcEvent->getApplication()->getServiceManager();
+        $eventManager   = $mvcEvent->getApplication()->getEventManager();
+        $sharedEventManager = $eventManager->getSharedManager();
+        // attach image shared event listener
+        $sharedEventManager->attachAggregate($serviceManager->get('FileManager\\Image\\SharedEventListener'));
+        $moduleRouteListener = new ModuleRouteListener();
+        $moduleRouteListener->attach($eventManager);
+        // set role based on oAuth client id
+        $eventManager->attach(
+            MvcAuthEvent::EVENT_AUTHENTICATION_POST,
+            function ($mvcAuthEvent) {
+                $identity     = $mvcAuthEvent->getIdentity();
+                $authIdentity = $identity->getAuthenticationIdentity();
+                if (!$identity instanceof \ZF\MvcAuth\Identity\GuestIdentity) {
+                    $identity->setName($authIdentity['client_id']);
+                }
+            },
+            100
+        );
+        // attach ACL for checking Scope
+        $eventManager->attach(
+            MvcAuthEvent::EVENT_AUTHORIZATION_POST,
+            $serviceManager->get('FileManager\\Image\\Authorization\\AclScopeListener'),
+            101
+        );
+        // attach ACL for checking Image Owner
+        $eventManager->attach(
+            MvcAuthEvent::EVENT_AUTHORIZATION_POST,
+            $serviceManager->get('FileManager\\Image\\Authorization\\AclImageListener'),
+            100
+        );
+    }
+    
+    public function getConfig()
+    {
+        return include __DIR__ . '/../../../config/module.config.php';
+    }
+
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'ZF\Apigility\Autoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__,
+                ),
+            ),
+        );
+    }
+}
